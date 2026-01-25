75408c249b75ba668b6049e056ec86e085b7bd2f
YazanJbara <90881981+YazanJbara@users.noreply.github.com>
2026-01-25 09:18:09 +0300

    added select components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import './App.css';
 import Header from './components/Header';
 import Information from './components/Information';
+import {
+  Select,
+  SelectContent,
+  SelectValue,
+  SelectTrigger,
+  SelectItem,
+  SelectSeparator,
+} from '@/components/ui/select';
+import { Separator } from '@/components/ui/separator';
 function App() {
   return (
-    <>
+    <main className='p-4'>
       <Header />
-      <section className='mt-8 grid grid-cols-[1fr_1px_1fr_1px_1fr] gap-5 text-center'>
+      <section className='mt-8 grid grid-cols-[1fr_1px_1fr_1px_1fr] gap-x-5 text-center'>
         <Information />
       </section>
-    </>
+      <Separator orientation='horizontal' className='mt-4' />
+    </main>
   );
 }
 
--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,4 +1,12 @@
 import { Separator } from './ui/separator';
+import {
+  Select,
+  SelectContent,
+  SelectValue,
+  SelectTrigger,
+  SelectItem,
+  SelectSeparator
+} from '@/components/ui/select';
 function Information() {
   return (
     <>
@@ -6,15 +14,39 @@ function Information() {
         <b className='text-preset-3-mobile block'>WPM:</b>
         <strong className='text-preset-2 text-neutral-0'>40</strong>
       </div>
-      <Separator orientation='vertical'  />
+      <Separator orientation='vertical' />
       <div className='space-y-2'>
         <b className='text-preset-3-mobile block'>Accuracy:</b>
         <strong className='text-preset-2 text-yellow-400'>94%</strong>
       </div>
-      <Separator orientation='vertical'  />
+      <Separator orientation='vertical' />
       <div className='space-y-2'>
         <b className='text-preset-3-mobile block'>Time:</b>
         <strong className='text-preset-2 text-red-500'>0:46</strong>
+      </div>
+      <div className='col-span-full mt-5 flex items-center justify-center gap-2.5'>
+        <Select>
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Hard' />
+          </SelectTrigger>
+          <SelectContent position='popper'>
+            <SelectItem value='Easy'>Easy</SelectItem>
+            <SelectSeparator />
+            <SelectItem value='normal'>Normal</SelectItem>
+            <SelectSeparator />
+            <SelectItem value='hard'>Hard</SelectItem>
+          </SelectContent>
+        </Select>
+        <Select>
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Timed' />
+          </SelectTrigger>
+          <SelectContent position='popper'>
+            <SelectItem value='timed'>Timed(60s)</SelectItem>
+            <SelectSeparator />
+            <SelectItem value='passage'>Passage</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
     </>
   );
